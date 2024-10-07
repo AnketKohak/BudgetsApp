9bec4dee0eaa79cebb0b4d12ad99a93ac9e75447
AnketKohak <anketkohak40@gmail.com>
2024-10-07 18:06:56 +0530

    Initial Commit

---
+++ BudgetsApp/BudgetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetsAppApp.swift
+//  BudgetsApp
+//
+//  Created by Anket Kohak on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
